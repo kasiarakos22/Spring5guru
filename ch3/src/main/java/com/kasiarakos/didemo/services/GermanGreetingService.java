c86819afcd7ebf04c1241d1a38cecf6e9f6651bd
dkasiaras <dimitris.kasiaras@agileactors.com>
2018-07-10 11:32:00 +0300

    37 Default Profile

--- ch3/src/main/java/com/kasiarakos/didemo/services/GermanGreetingService.java
+++ ch3/src/main/java/com/kasiarakos/didemo/services/GermanGreetingService.java
@@ -1,16 +1,15 @@
 package com.kasiarakos.didemo.services;
 
-
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile(value = "de")
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService{
+public class GermanGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello from PrimaryService";
+        return "Hallo Welt";
     }
 }
