9eaa32c760e3131b25b7bb6a18ae3c14ca3f104b
dkasiaras <dimitris.kasiaras@camelotglobal.com>
2018-09-19 18:05:01 +0300

    95. Spring Environment properties

---
+++ ch6/src/main/java/guru/springframework/config/PropertyConfig.java
@@ -0,0 +1,35 @@
+package guru.springframework.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import guru.springframework.examplebeans.FakeDatasource;
+
+@Configuration
+@PropertySource("classpath:datasource.properties")
+public class PropertyConfig {
+
+    @Value("${guru.username}")
+    String user;
+    @Value("${guru.password}")
+    String password;
+    @Value("${guru.dburl}")
+    String url;
+
+    @Bean
+    public FakeDatasource fakeDatasource(){
+        FakeDatasource fakeDatasource = new FakeDatasource();
+        fakeDatasource.setPassword(password);
+        fakeDatasource.setUrl(url);
+        fakeDatasource.setUser(user);
+        return fakeDatasource;
+    }
+
+   /* @Bean
+    public static PropertySourcesPlaceholderConfigurer properties(){
+        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        return propertySourcesPlaceholderConfigurer;
+    }*/
+}
